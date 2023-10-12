3924abb24354d18742a35752a0651b328921cfb6
vitorvaske6 <vitorvaske6@gmail.com>
2023-10-12 16:56:11 -0300

    20231012.0 - Aula 1 - Next Link e SPA

---
+++ pages/404.js
@@ -0,0 +1,12 @@
+import Link from "../src/components/Link";
+
+export default function Page404() {
+    return (
+        <div>
+            <h1>A página que você está procurando não existe!</h1>
+            <Link href="/">
+                Ir para o Home
+            </Link>
+        </div>
+    )
+}
---
+++ src/components/Link/index.js
@@ -0,0 +1,12 @@
+import NextLink from "next/link"
+import LinkEstilizado from "../LinkEstilizado"
+
+export default function Link({children, href, ...props}) {
+    return (
+        <NextLink href={href} passHref>
+            <LinkEstilizado {...props}>
+                {children}
+            </LinkEstilizado>
+        </NextLink>
+    )
+}
